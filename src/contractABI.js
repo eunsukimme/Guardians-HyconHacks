3ff3a401cae4fd94dd6784c3468cd2b05e1a1754
KIM <glafurgkswk3@naver.com>
2018-09-14 17:22:26 +0900

    Edit some function

--- src/contractABI.js
+++ src/contractABI.js
@@ -1,1 +1,210 @@
-const contractABI = [];
+const contractABI = [
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_userAccount",
+                "type": "address"
+            },
+            {
+                "name": "_minType",
+                "type": "uint8"
+            },
+            {
+                "name": "_name",
+                "type": "string"
+            }
+        ],
+        "name": "addUser",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "previousOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipRenounced",
+        "type": "event"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "addressToMinority",
+        "outputs": [
+            {
+                "name": "userAccount",
+                "type": "address"
+            },
+            {
+                "name": "minType",
+                "type": "uint8"
+            },
+            {
+                "name": "name",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_userAccount",
+                "type": "address"
+            }
+        ],
+        "name": "checkUser",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_userAccount",
+                "type": "address"
+            }
+        ],
+        "name": "getUser",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "name": "userAccount",
+                        "type": "address"
+                    },
+                    {
+                        "name": "minType",
+                        "type": "uint8"
+                    },
+                    {
+                        "name": "name",
+                        "type": "string"
+                    }
+                ],
+                "name": "_minor",
+                "type": "tuple"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "minorCount",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "minorities",
+        "outputs": [
+            {
+                "name": "userAccount",
+                "type": "address"
+            },
+            {
+                "name": "minType",
+                "type": "uint8"
+            },
+            {
+                "name": "name",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
--- src/web3processing.js
+++ src/web3processing.js
@@ -1,8 +1,8 @@
-let contract;
+let Guardian;
 let userAccount;
 
 const startApp = () => {
-    const contractAddress = '';
+    const contractAddress = '0x0287b9f53ba465e4a2e3f59d78e26ac90d9c504f';
     Guardian = new web3.eth.Contract(contractABI, contractAddress);
 
     let checkAccountChange = setInterval(async function() {
@@ -13,11 +13,13 @@ const startApp = () => {
             // 새 계정에 대한 UI로 업데이트하기 위한 함수 호출
             //alert('Your account is ' + userAccount);
             // 해당 계정의 유저가 이미 존재하는지 확인
-            const isUserExist = Guardian.methods.checkUser(userAccount).call();
+            //console.log(Guardian.methods.checkUser(userAccount).call());
+            console.log(checkUser());
+            //console.log(isUserExist);
             // 유저가 존재한다면 환영한다
-            if(isUserExist){
-                const user = Guardian.methods.getUser(userAccount).call();
-                console.log(`Hello, ${user.name}!`);
+            if(checkUser()){
+                console.log(getUser());
+                //console.log(`Hello, ${user.name}!`);
             } else {
                 // 가입을 권유한다
                 console.log('You should create a new account');
@@ -27,8 +29,18 @@ const startApp = () => {
     }, 1000);
 };
 
+const checkUser = () => {
+    return Guardian.methods.checkUser(userAccount).call().then((result)=> {
+        console.log(result);
+        return result;
+    })
+}
+
 const getUser = () => {
-    return Guardian.methods.getUser(userAccount).call();
+    return Guardian.methods.getUser(userAccount).call().then(function(result){
+        console.log(result);
+        return result;
+    })
 }
 
 const addUser = (_minType, _name) => {
@@ -47,8 +59,10 @@ const addUser = (_minType, _name) => {
 
 window.addEventListener('load', function() {
     if(typeof web3 !== 'undefined') {
+        //alert('메타마스크가 주입되었습니다');
         web3 = new Web3(web3.currentProvider);
     } else {
+        //alert('메타마스크 계정을 찾을 수 없습니다.');
         web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
     }
 
